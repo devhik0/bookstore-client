6f648daf5f5246a6fe7ac273ec9f0121b7be428f
hikmet <aydin96tr@gmail.com>
2023-11-26 15:23:02 +0300

    config, add ui lib, add icons and edit

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,30 @@
-import Link from "next/link"
+import { Button } from "@/components/ui/button";
+import { Camera } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home() {
-  const data = await fetch("http://localhost:8080/api/books", {})
-  const books = await data.json()
+  const data = await fetch("http://localhost:8080/api/books", {});
+  const books = await data.json();
 
   // console.log("Books: ", books)
 
   return (
-    <>Bookstore Test Here data comes from server:
-    <div style={{border: "1px solid green", padding: "1rem"}}>
-     {books.map(book => <div key={book.id} style={{border: "1px solid blue", margin: "1rem", padding: "1rem"}}>
-      <h3>{book.title}</h3>
-      <span>{book.yearPublished}</span>
-      <p>{book.description}</p>
-      <span>{book.numPages}</span>
-     </div>)}
-    </div>
-     <Link href={"/staff"} className="bg-green-400 border border-green-400">Go to staff</Link>
+    <>
+      Bookstore Test Here data comes from server:
+      <div style={{ border: "1px solid green", padding: "1rem" }}>
+        {books.map((book) => (
+          <div key={book.id} style={{ border: "1px solid blue", margin: "1rem", padding: "1rem" }}>
+            <h3>{book.title}</h3>
+            <span>{book.yearPublished}</span>
+            <p>{book.description}</p>
+            <span>{book.numPages}</span>
+          </div>
+        ))}
+      </div>
+      <Button>
+        <Link href={"/staff"}>Go to staff</Link>
+      </Button>
+      <Camera color="red" size={48} />
     </>
-  )
+  );
 }
--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -1,19 +1,26 @@
-import Link from "next/link"
+import Link from "next/link";
 
 const Staff = async () => {
-
-  const data = await fetch("http://localhost:8080/api/staff")
-  const staff = await data.json()
+  const data = await fetch("http://localhost:8080/api/staff");
+  const staff = await data.json();
 
   // console.log(staff)
 
-  return (<>Staff page <br /> <button className="bg-blue-400">Add staff
+  return (
+    <>
+      Staff page <br /> <button className="bg-blue-400">Add staff</button>
+      <div>
+        {staff.map((staff) => (
+          <div key={staff.id} className="border border-orange-400 m-4 p-4">
+            {staff.fullName}
+          </div>
+        ))}
+      </div>
+      <Link href={"/"} className="bg-green-400">
+        Back to books
+      </Link>
+    </>
+  );
+};
 
-  </button>
-    <div>{staff.map(staff => <div key={staff.id} className="border border-orange-400 m-4 p-4">{staff.fullName}</div>)}</div>
-
-    <Link href={"/"} className="bg-green-400">Back to books</Link>
-  </>)
-}
-
-export default Staff
+export default Staff;
