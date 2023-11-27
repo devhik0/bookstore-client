f40fd334e23a8384022256d198f763d48bac4700
hikmet <aydin96tr@gmail.com>
2023-11-27 20:32:09 +0300

    image upload ?

--- app/staff/page.tsx
+++ app/staff/page.tsx
@@ -6,6 +6,45 @@ const Staff = async () => {
 
   // console.log(staff)
 
+  // Image Upload
+  // const uploadImage = async (file) => {
+  //   "use server";
+  //   const bookData = JSON.stringify({
+  //     title: "The Silent Observer",
+  //     authorNameList: ["Gillian Flynn"],
+  //     genreTagList: ["Thriller", "Mystery"],
+  //     publisher: "Whispering Pages",
+  //     yearPublished: 2022,
+  //     description: "A gripping tale of secrets and suspense",
+  //     language: "ENGLISH",
+  //     numPages: 420,
+  //     priceBeforeDiscount: 15,
+  //     discountPercent: 10,
+  //     copiesAvailable: 300,
+  //   });
+
+  //   const formData = new FormData();
+  //   // formData.append("bookDTO", bookData);
+  //   formData.append("bookDTO", new Blob([JSON.stringify(bookData)], { type: "application/json" }));
+  //   // formData.append("imageJpg", file, "origin.jpg");
+  //   formData.append("imageJpg", file, "origin.jpg");
+
+  //   try {
+  //     const res = await fetch("http://localhost:8080/api/books", {
+  //       method: "POST",
+  //       body: formData,
+  //       redirect: "follow",
+  //     });
+
+  //     const imgRes = await res.text();
+  //     console.log("Uploaded: ", imgRes);
+  //   } catch (error) {
+  //     console.error("Upload Error: ", error);
+  //   }
+  // };
+
+  // await uploadImage();
+
   return (
     <>
       Staff page <br /> <button className="bg-blue-400">Add staff</button>
@@ -19,6 +58,7 @@ const Staff = async () => {
       <Link href={"/"} className="bg-green-400">
         Back to books
       </Link>
+      {/* <Upload upload={uploadImage} /> */}
     </>
   );
 };
