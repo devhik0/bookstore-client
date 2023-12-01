f5bdf673b90a348462b76369cd9599442cd5ccfd
hikmet <aydin96tr@gmail.com>
2023-12-02 02:17:13 +0300

    add searchbar + bookdetails

--- app/books/[id]/page.tsx
+++ app/books/[id]/page.tsx
@@ -1,3 +1,51 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default async function Book({ params }) {
-  return <div>Book Details {params.id}</div>;
+  const data = await fetch(`http://localhost:8080/api/books/${params.id}`);
+  const book = await data.json();
+
+  const discountAmount = ((book.priceBeforeDiscount || 50) * book.discountPercent) / 100;
+  const discountedPrice = (book.priceBeforeDiscount || 50) - discountAmount;
+
+  return (
+    <div className="p-4 border border-orange-900 m-4">
+      <h3 className="text-2xl">Book Details</h3>
+      <div className="p-4">
+        <Image src={book.imageLink} alt="book-img" width={100} height={100} />
+        <h3 className="text-lg">Title: {book.title}</h3>
+        <div className="p-4">
+          {book.authorNameList.map((author, idx) => (
+            <span className="bg-green-200 p-2" key={idx}>
+              {author}
+            </span>
+          ))}
+        </div>
+        <div className="p-4">
+          {book.genreTagList.map((genre, idx) => (
+            <span className="bg-purple-200 p-2" key={idx}>
+              {genre}
+            </span>
+          ))}
+        </div>
+        <div className="flex gap-4 flex-col w-[10%]">
+          <span className="bg-cyan-400 p-2">{book.publisher}</span>
+          <span className="p-2 bg-gray-300">{book.yearPublished}</span> <br />
+          <span className="p-2 bg-red-300">{book.language}</span> <br />
+          <span className="p-2 bg-yellow-300">{book.numPages}</span> <br />
+        </div>
+        <p className="m-4 p-4 bg-gray-200">{book.description || "A book desc"}</p>
+        <div className="flex flex-row gap-2 items-center my-4">
+          <span className="line-through">$ {book.priceBeforeDiscount || 50}</span>
+          <span className="text-red-500"> -%{book.discountPercent}</span> <br />
+          <span className="bg-lime-200 p-2">$ {discountedPrice}</span>
+        </div>
+        <span className="mt-4 p-2 border border-orange-800">{book.copiesAvailable} Copies</span>
+      </div>
+      <Link href={`/cart`} className="mt-2">
+        <Button className="bg-orange-800">Add to Cart</Button>
+      </Link>
+    </div>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,42 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
 
+// TODO: Add searchbar here
+
 export default async function Home() {
+  // const query = {
+  //   title: "sdfg",
+  // };
+
+  // const search = async () => {
+  //   "use server";
+  //   const formData = new FormData();
+  //   const data = formData.get("query");
+  //   console.log("Book: ", data);
+  // };
+
   return (
     <div className="w-full h-full">
       <div className="">
-        <Image src={"/hero-banner.jpg"} width={1400} height={500} alt="hero-banner" className="opacity-100" />
+        <Image src={"/hero-banner.jpg"} width={1400} height={500} alt="hero-banner" className="opacity-40" />
         <div className="absolute top-[12%] left-[20%]">
           <h1 className="text-5xl font-bold text-orange-900">
             Find your favorite book at your fingertips,
-            <br /> <span className="text-gray-100 underline">in seconds</span>
+            <br /> <span className="text-gray-800 underline">in seconds</span>
           </h1>
         </div>
-        <Link href={"/books"}>
-          <Button className="absolute top-[40%] left-[40%] mt-4 p-4 text-xl w-[20%] bg-orange-800">Search now</Button>
-        </Link>
+
+        <div className="absolute top-[30%] left-[33%] mt-4 bg-orange-800">
+          {/* <form action={search} className="border-2 border-green-800 p-4 flex gap-4 justify-between items-center">
+            <label className="w-full">Search for...</label>
+            <div className="flex gap-4 w-full">
+              <input name="query" type="search" placeholder="Search for title or author" className="p-2" />
+              <Link href={`/books?title=${query.title}`}>
+                <Button type="submit">Search</Button>
+              </Link>
+            </div>
+          </form> */}
+        </div>
         {/* for testing only ! */}
         <Link href={"/staff"} className="absolute top-[50%] left-[40%] mt-4 p-4 text-xl w-[20%] bg-green-800">
           Staff
