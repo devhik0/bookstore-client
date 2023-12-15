e04d28f148ea8e46024d50746b2f60d0c7cb6bb4
hikmet <aydin96tr@gmail.com>
2023-12-15 22:12:21 +0300

    cache fix

--- app/_actions/loginUser.ts
+++ app/_actions/loginUser.ts
@@ -1,14 +1,17 @@
 import { BASE_URL } from "@/lib/constants";
+import { unstable_noStore } from "next/cache";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const loginUser = async (formData: FormData) => {
   "use server";
 
+  unstable_noStore(); // to get different token everytime
+
   try {
     const loginData = {
-      email: formData.get("email"),
-      password: formData.get("password"),
+      email: formData.get("email") as string,
+      password: formData.get("password") as string,
     };
     const data = await fetch(`${BASE_URL}/authentication`, {
       method: "POST",
@@ -22,7 +25,10 @@ export const loginUser = async (formData: FormData) => {
       console.log("Error while login");
     }
 
-    const res = await data.json();
+    const res = (await data.json()) as { token: string };
+
+    console.log("Token: ", res.token);
+
     cookies().set("auth_token", res.token, { sameSite: "strict", secure: true, httpOnly: true });
   } catch (error) {
     console.log(error);
